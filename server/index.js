76faf1220f4b94f97c87bd322c89121b3e139504
Cyber_Infernox <sayonkar@gmail.com>
2024-06-30 20:10:17 +0530

    Added signup route and controller

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 import dotenv from "dotenv";
-
 import express from "express";
 import mongoose from "mongoose";
+
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 // express app
 const app = express();
+
+// for sending data as json to server
+app.use(express.json());
 
 // connect to database
 mongoose
@@ -20,3 +24,9 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "Pinging..." });
+});
+
+app.use("/api/auth", authRoutes);
